7a75f579a1ba5fe56dacf78e878e3a172edef350
Ilham Fadhilah <r.ilhamfadhilah@gmail.com>
2025-01-31 00:13:37 +0700

    Updates

---
+++ precache-manifest.b34ee5e5f9492e36dfff42ff24248ee8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca08ff96da9d84a9c08a923ac508ecb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8e6ba79084e44a3096d",
+    "url": "/static/css/2.fdde62ca.chunk.css"
+  },
+  {
+    "revision": "b1a23bdf081bc8f3c1cd",
+    "url": "/static/css/main.e3c6e229.chunk.css"
+  },
+  {
+    "revision": "a8e6ba79084e44a3096d",
+    "url": "/static/js/2.3083fe14.chunk.js"
+  },
+  {
+    "revision": "26ffede4364af2f0a8d2bdf998fab508",
+    "url": "/static/js/2.3083fe14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1a23bdf081bc8f3c1cd",
+    "url": "/static/js/main.8d052956.chunk.js"
+  },
+  {
+    "revision": "c044c296fef9e582399f",
+    "url": "/static/js/runtime-main.4070baf2.js"
+  },
+  {
+    "revision": "7c4aed56245ae966a682cb5ab46dee43",
+    "url": "/static/media/profile.7c4aed56.png"
+  }
+]);
